59f785db41474fdbd001aadb2e54f21c1a137dc5
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2023-01-30 09:19:28 -0300

    grains exercise done

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,14 @@
-def square(number):
-    pass
+"""Implementation of grains exercise"""
+MIN_SQUARE = 1
+MAX_SQUARE = 64
 
 
-def total():
-    pass
+def square(number: int) -> int:
+    if not MIN_SQUARE <= number <= MAX_SQUARE:
+        raise ValueError("square must be between 1 and 64")
+
+    return 1 << (number - 1)
+
+
+def total() -> int:
+    return (1 << MAX_SQUARE) - 1
