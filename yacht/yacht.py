bf433eba5644a10e146f8cbbc2eaffe43509ffe2
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2023-01-30 08:31:00 -0300

    change yacht implementation

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 YACHT = (lambda d: 50 if len(set(d)) == 1 else 0)
 ONES = (lambda d: digits(d, 1))
 TWOS = (lambda d: digits(d, 2))
@@ -10,17 +8,14 @@ SIXES = (lambda d: digits(d, 6))
 FULL_HOUSE = (lambda d: sum(d) if len(set(d)) == 2 and any(
     d.count(x) == 3 for x in set(d)) else 0)
 FOUR_OF_A_KIND = (lambda d: sum(x * 4 for x in set(d) if d.count(x) >= 4))
-LITTLE_STRAIGHT = (lambda d: 30 if sorted(d) == [1, 2, 3, 4, 5] else 0)
-BIG_STRAIGHT = (lambda d: 30 if sorted(d) == [2, 3, 4, 5, 6] else 0)
+LITTLE_STRAIGHT = (lambda d: 30 if len(set(d)) == 5 and 6 not in d else 0)
+BIG_STRAIGHT = (lambda d: 30 if len(set(d)) == 5 and 1 not in d else 0)
 CHOICE = sum
 
 
-DICE = list[int]
-
-
-def digits(dice: DICE, digit: int):
+def digits(dice, digit):
     return dice.count(digit) * digit
 
 
-def score(dice: DICE, category: Callable):
+def score(dice, category):
     return category(dice)
