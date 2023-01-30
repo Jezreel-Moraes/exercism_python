68e8092093396f1a11135db7fc5275b574d208c2
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2023-01-30 11:32:01 -0300

    diffie-hellman exercise done

--- diffie-hellman/diffie_hellman.py
+++ diffie-hellman/diffie_hellman.py
@@ -1,10 +1,15 @@
-def private_key(p):
-    pass
+"""Diffie Hellman exercise implementation"""
+
+import secrets
 
 
-def public_key(p, g, private):
-    pass
+def private_key(p: int) -> int:
+    return secrets.randbelow(p - 2) + 2
 
 
-def secret(p, public, private):
-    pass
+def public_key(p: int, g: int, private: int) -> int:
+    return pow(g, private, p)
+
+
+def secret(p: int, public: int, private: int) -> int:
+    return pow(public, private, p)
