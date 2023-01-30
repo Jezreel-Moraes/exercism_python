a3c026e9385bccaa8156fe331d6b59f5cc372cd7
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2023-01-30 10:22:53 -0300

    difference_of_squares exercise done

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,13 @@
-def square_of_sum(number):
-    pass
+"""difference of squares exercise implementation"""
 
 
-def sum_of_squares(number):
-    pass
+def square_of_sum(number: int) -> int:
+    return int((number * (number + 1) / 2) ** 2)
 
 
-def difference_of_squares(number):
-    pass
+def sum_of_squares(number: int) -> int:
+    return number * (number+1) * (2 * number + 1) // 6
+
+
+def difference_of_squares(number: int) -> int:
+    return square_of_sum(number) - sum_of_squares(number)
