63bebc9eac55b65225bced3dfda0e66e324e5022
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2023-01-30 22:52:38 -0300

    triangle exercise done

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,29 @@
-def equilateral(sides):
-    pass
+"""triangle exercise implementation"""
+
+from typing import Callable
+
+SIDES = list[float]
 
 
-def isosceles(sides):
-    pass
+def is_triangle(func: Callable) -> Callable:
+    def inner(sides: SIDES) -> bool:
+        sum_ = sum(sides)
+        is_valid = not any(x > sum_ - x if x != 0 else True for x in sides)
+        return is_valid and func(sides)
+
+    return inner
 
 
-def scalene(sides):
-    pass
+@is_triangle
+def equilateral(sides: SIDES) -> bool:
+    return len(set(sides)) == 1
+
+
+@is_triangle
+def isosceles(sides: SIDES) -> bool:
+    return len(set(sides)) <= 2
+
+
+@is_triangle
+def scalene(sides: SIDES) -> bool:
+    return len(set(sides)) == 3
